62cd3af38071c2a9cfbe4ed11c5cc992adc0d704
mohammad javad <mohammad javad@desktop.com>
2021-06-07 13:08:29 +0430

    login users

--- t1/main.cpp
+++ t1/main.cpp
@@ -9,6 +9,8 @@
 #include <fstream>
 #define Movies "Movies.txt"
 #define Nf "Nf.txt"
+#define User "user.txt"
+#define Admin "admin.txt"
 using namespace std;
 typedef string st;
 class Movie {
@@ -28,7 +30,6 @@ public:
 };
 int main(void)
 {
-    cout << "Hi" << endl ;
     QList<Movie> q;
     QList<Movie> qq;
     QMap<string,QList<Movie>> sq;
@@ -36,14 +37,15 @@ int main(void)
     QVector<string> actors;
     string in , name, director;
     bool st =false;
-    //reading file
+
     fstream file;
-    file.open(Nf,ios::in | ios::out);
+    file.open(Nf,ios::in | ios::out | ios::app);
     if(file.is_open()){
         file >> nff;
     }
     file.close();
     fstream qst;
+    //add to qlist
     qst.open(Movies,ios::in | ios::out);
     if(qst.is_open()){
         string tp="" , tmp;
@@ -78,190 +80,324 @@ int main(void)
         }
         qst.close();
     }
+    int enter=0;
+    cout << "Hi"<< endl;
+    cout << "If you are admin enter a, if you are user enter u and if you are new user enter n"<< endl;
+    string user , pass ,  iii;
+    cin >> iii;
+    if(iii=="a")
+    {
+        cout << "enter your username:";
+        cin >> user;
+        cout << "enter your password:";
+        cin >> pass;
+        fstream af;
+        af.open(Admin,ios::in | ios::out);
+        if(af.is_open())
+        {
+            string tp="" , tmp1,tmp2;
+            while(getline(af,tp)){
+                int uname = tp.find("username:");
+                for(int  j=uname+9;tp[j]!='/';j++)
+                    tmp1+=tp[j];
+                int ipass = tp.find("password:");
+                for(int j=ipass+9;tp[j]!='/';j++)
+                    tmp2+=tp[j];
+                if(user==tmp1 && pass==tmp2)
+                {
+                    cout << "Welcome admin" <<endl;
+                    enter =1;
+                    while(1){
+                        //update movies file
+                        if(st==true)
+                        {
+                            i=0;
+                            fstream qst;
+                            qst.open(Movies,ios::in | ios::out |ios::trunc);
+                            for(auto itr = q.begin();itr!=q.end();itr ++,i++)
+                            {
+                                qst <<"name:"<<(*itr).name<<"/"
+                                   <<"director:"<<(*itr).director<<"/"
+                                  <<"actors:"<<(*itr).actors.at(i)<<"/"
+                                 <<"year:"<<(*itr).year<<"/"
+                                <<"capacity:"<<(*itr).capacity<<"/"
+                                <<endl;
 
-    while(1){
-        //update movies file
-        if(st==true)
-        {
-            i=0;
-            fstream qst;
-            qst.open(Movies,ios::in | ios::out |ios::trunc);
-            for(auto itr = q.begin();itr!=q.end();itr ++,i++)
-            {
-                qst <<"name:"<<(*itr).name<<"/"
-                   <<"director:"<<(*itr).director<<"/"
-                  <<"actors:"<<(*itr).actors.at(i)<<"/"
-                 <<"year:"<<(*itr).year<<"/"
-                <<"capacity:"<<(*itr).capacity<<"/"
-                <<endl;
+                            }
+                            st=false;
+                        }
+                        cout << "Enter + for add, - for remove, p for print, g for make group, e for edit a film, f for finish " << endl;
+                        cin >> in;
+                        if(in=="+")
+                        {
+                            nff++;
+                            ofstream file;
+                            file.open(Nf,ios::in | ios::out);
+                            file << nff;
+                            file.close();
+                            ofstream qst;
+                            qst.open(Movies , ios::in | ios::app );
+                            cout << "Enter the name of film : ";
+                            cin >> name ;
+                            qst <<"name:" << name << "/" ;
+                            cout << "Enter the director of film : ";
+                            cin >> director;
+                            qst << "director:"<<director<<"/";
+                            cout << "How many actors dose it have ? ";
+                            cin >> na;
+                            if(na>0)
+                            {
+                                cout << "Enter the name of actors" << endl;
+                                string actor;
+                                qst << "actors:";
+                                for(int i=0;i<na;i++)
+                                {
+                                    cin >> actor;
+                                    actors.push_back(actor);
+                                    qst << actor << " ";
+                                }
+                                qst << "/";
+                            }
+                            cout << "In wich year this file was created ? ";
+                            cin >> year ;
+                            qst << "year:"<<year<<"/";
+                            cout << "Enter the capacity of film : ";
+                            cin >> capacity;
+                            qst << "capacity:"<<capacity<<"/";
+                            Movie a(name,director,actors,year,capacity);
+                            q.push_back(a);
+                            qst <<endl;
+                            qst.close();
+                        }
+                        else if(in=="p")
+                        {
 
-            }
-            st=false;
-        }
-        cout << "Enter + for add, - for remove, p for print, g for make group, e for edit a film " << endl;
-        cin >> in;
-        if(in=="+")
-        {
-            nff++;
-            ofstream file;
-            file.open(Nf,ios::in | ios::out);
-            file << nff;
-            file.close();
-            ofstream qst;
-            qst.open(Movies , ios::in | ios::app );
-            cout << "Enter the name of film : ";
-            cin >> name ;
-            qst <<"name:" << name << "/" ;
-            cout << "Enter the director of film : ";
-            cin >> director;
-            qst << "director:"<<director<<"/";
-            cout << "How many actors dose it have ? ";
-            cin >> na;
-            if(na>0)
-            {
-                cout << "Enter the name of actors" << endl;
-                string actor;
-                qst << "actors:";
-                for(int i=0;i<na;i++)
-                {
-                    cin >> actor;
-                    actors.push_back(actor);
-                    qst << actor << " ";
-                }
-                qst << "/";
-            }
-            cout << "In wich year this file was created ? ";
-            cin >> year ;
-            qst << "year:"<<year<<"/";
-            cout << "Enter the capacity of film : ";
-            cin >> capacity;
-            qst << "capacity:"<<capacity<<"/";
-            Movie a(name,director,actors,year,capacity);
-            q.push_back(a);
-            qst <<endl;
-            qst.close();
-        }
-        else if(in=="p")
-        {
+                            if(q.size()>0)
+                            {
+                                i=1;
+                                for(auto itr = q.begin();itr!=q.end();itr ++ ,i++)
+                                {
+                                    cout <<i;
+                                    cout <<": ";
+                                    cout <<(*itr).name << endl;
+                                }
+                                i=1;
+                            }
+                            else
+                                cout << "No films are available" << endl;
+                        }
+                        else if(in=="-")
+                        {
+                            if(q.size()>0)
+                            {
+                                nff--;
+                                ofstream file;
+                                file.open(Nf,ios::in | ios::out);
+                                file << nff;
+                                file.close();
+                                i=1;
+                                cout << "The films are here : " << endl;
+                                for(auto itr = q.begin();itr!=q.end();itr ++,i++)
+                                {
+                                    cout <<i<<": " <<(*itr).name << endl;
+                                }
+                                i=1;
+                                cout << "Enter the number of movie : ";
+                                int rm;
+                                cin >> rm;
+                                q.removeAt(rm-1);
+                                i=1;
+                                for(auto itr = q.begin();itr!=q.end();itr ++,i++)
+                                {
+                                    cout <<i<<": " <<(*itr).name << endl;
 
-            if(q.size()>0)
-            {
-                i=1;
-                for(auto itr = q.begin();itr!=q.end();itr ++ ,i++)
-                {
-                    cout <<i;
-                    cout <<": ";
-                    cout <<(*itr).name << endl;
+                                }
+                                st=true;
+                                nff--;
+                            }
+                            else
+                                cout << "No films are available" << endl;
+                        }
+                        else if(in=="g")
+                        {
+                            i=1;
+                            cout << "The films are : " << endl;
+                            for(auto itr = q.begin();itr!=q.end();itr ++,i++)
+                            {
+                                cout <<i<<": " <<(*itr).name << endl;
+                            }
+                            i=1;
+                            cout << "Enter the name of group : ";
+                            string ng;
+                            cin >> ng;
+                            cout << "How many films do you want to be in a group : ";
+                            int nug;
+                            cin >> nug;
+                            int number;
+                            qq.clear();
+                            for(int j=0;j<nug;j++)
+                            {
+                                cout << "enter the " << j+1<<" member : ";
+                                cin >> number;
+                                qq.push_back(q.operator [](number-1));
+                            }
+                            sq[ng] = qq;
+                            QMapIterator<string,QList<Movie>> ii(sq);
+                            while (ii.hasNext()) {
+                                i=0;
+                                ii.next();
+                                cout << ii.key() << ":" ;
+                                for(auto it = ii.value().begin();it!=ii.value().end();it++)
+                                {
+                                    cout << ii.value().at(i).name <<" ";
+                                    i++;
+                                }
+                                cout << endl;
+                            }
+                        }
+                        else if(in=="e")
+                        {
+                            i=1;
+                            cout << "The films are : " << endl;
+                            for(auto itr = q.begin();itr!=q.end();itr ++,i++)
+                            {
+                                cout <<i<<": " <<(*itr).name << endl;
+                            }
+                            i=1;
+                            cout << "enter the number of film : ";
+                            int nf;
+                            cin >> nf;
+                            q.removeAt(nf-1);
+                            cout << "Enter the name of film : ";
+                            cin >> name ;
+                            cout << "Enter the director of film : ";
+                            cin >> director;
+                            cout << "How many actors dose it have ? ";
+                            cin >> na;
+                            if(na>0)
+                                cout << "Enter the name of actors" << endl;
+                            string actor;
+                            for(int i=0;i<na;i++)
+                            {
+                                cin >> actor;
+                                actors.push_back(actor);
+                            }
+                            cout << "In wich year this file was created ? ";
+                            cin >> year;
+                            cout << "Enter the capacity of film : ";
+                            cin >> capacity;
+                            Movie a(name,director,actors,year,capacity);
+                            q.push_back(a);
+                            st = true;
+                        }
+                        else if(in=="f")
+                            break;
+                    }
                 }
-                i=1;
+                tmp1="";
+                tmp2=tmp1;
+                cout << "username or password is wrong" << endl;
+                main();
             }
-            else
-                cout << "No films are available" << endl;
-        }
-        else if(in=="-")
-        {
-            if(q.size()>0)
-            {
-                nff--;
-                ofstream file;
-                file.open(Nf,ios::in | ios::out);
-                file << nff;
-                file.close();
-                i=1;
-                cout << "The films are here : " << endl;
-                for(auto itr = q.begin();itr!=q.end();itr ++,i++)
-                {
-                    cout <<i<<": " <<(*itr).name << endl;
-                }
-                i=1;
-                cout << "Enter the number of movie : ";
-                int rm;
-                cin >> rm;
-                q.removeAt(rm-1);
-                i=1;
-                for(auto itr = q.begin();itr!=q.end();itr ++,i++)
-                {
-                    cout <<i<<": " <<(*itr).name << endl;
-
-                }
-                st=true;
-                nff--;
-            }
-            else
-                cout << "No films are available" << endl;
-        }
-        else if(in=="g")
-        {
-            i=1;
-            cout << "The films are : " << endl;
-            for(auto itr = q.begin();itr!=q.end();itr ++,i++)
-            {
-                cout <<i<<": " <<(*itr).name << endl;
-            }
-            i=1;
-            cout << "Enter the name of group : ";
-            string ng;
-            cin >> ng;
-            cout << "How many films do you want to be in a group : ";
-            int nug;
-            cin >> nug;
-            int number;
-            qq.clear();
-            for(int j=0;j<nug;j++)
-            {
-                cout << "enter the " << j+1<<" member : ";
-                cin >> number;
-                qq.push_back(q.operator [](number-1));
-            }
-            sq[ng] = qq;
-            QMapIterator<string,QList<Movie>> ii(sq);
-            while (ii.hasNext()) {
-                i=0;
-                ii.next();
-                cout << ii.key() << ":" ;
-                for(auto it = ii.value().begin();it!=ii.value().end();it++)
-                {
-                    cout << ii.value().at(i).name <<" ";
-                    i++;
-                }
-                cout << endl;
-            }
-        }
-        else if(in=="e")
-        {
-            i=1;
-            cout << "The films are : " << endl;
-            for(auto itr = q.begin();itr!=q.end();itr ++,i++)
-            {
-                cout <<i<<": " <<(*itr).name << endl;
-            }
-            i=1;
-            cout << "enter the number of film : ";
-            int nf;
-            cin >> nf;
-            q.removeAt(nf-1);
-            cout << "Enter the name of film : ";
-            cin >> name ;
-            cout << "Enter the director of film : ";
-            cin >> director;
-            cout << "How many actors dose it have ? ";
-            cin >> na;
-            if(na>0)
-                cout << "Enter the name of actors" << endl;
-            string actor;
-            for(int i=0;i<na;i++)
-            {
-                cin >> actor;
-                actors.push_back(actor);
-            }
-            cout << "In wich year this file was created ? ";
-            cin >> year;
-            cout << "Enter the capacity of film : ";
-            cin >> capacity;
-            Movie a(name,director,actors,year,capacity);
-            q.push_back(a);
-            st = true;
         }
     }
-    return 0;
+    else if(iii=="n")
+    {
+        cout << "enter your username:";
+        cin >> user;
+        cout << "enter your password:";
+        cin >> pass;
+        fstream uf;
+        uf.open(User,ios::in | ios::out|ios::app);
+        uf << "username:"<<user<<"/"<<"password:"<<pass<<"/"<<endl;
+        enter =1;
+        uf.close();
+    }
+    else if(iii=="u") {
+        cout << "enter your username:";
+        cin >> user;
+        cout << "enter your password:";
+        cin >> pass;
+        fstream uf;
+        uf.open(User,ios::in | ios::out);
+        if(uf.is_open())
+        {
+            string tp="" , tmp1,tmp2;
+            while(getline(uf,tp)){
+                int uname = tp.find("username:");
+                for(int  j=uname+9;tp[j]!='/';j++)
+                    tmp1+=tp[j];
+                int ipass = tp.find("password:");
+                for(int j=ipass+9;tp[j]!='/';j++)
+                    tmp2+=tp[j];
+                if(user==tmp1 && pass==tmp2)
+                {
+                    enter =1;
+                    break;
+                }
+                tmp1="";
+                tmp2=tmp1;
+            }
+            if(enter==0)
+            {
+                cout << "username or password is wrong" << endl;
+                main();
+            }
+        }
+    }
+if(enter==1)
+{
+    cout << "Hi " << user << endl ;
+    //print movies
+    if(q.size()>0)
+    {
+        cout << "Movise are : " <<endl;
+        i=1;
+        for(auto itr = q.begin();itr!=q.end();itr ++ ,i++)
+        {
+            cout <<i;
+            cout <<": ";
+            cout <<(*itr).name <<":" << (*itr).capacity  << endl;
+        }
+        i=1;
+    }
+    else
+    {
+        cout << "No films are available" << endl;
+        break;
+    }
+    cout << "choose a film" << endl;
+    int ch;
+    cin >> ch;
+    ch--;
+    int h=q.at(ch).capacity;
+    if(h<=0)
+    {
+        cout << "it's impossible to choose, try again" <<endl;
+        main();
+    }
+    h--;
+    Movie a(q.at(ch).name,q.at(ch).director,q.at(ch).actors,q.at(ch).year,h);
+    q.replace(ch,a);
+    st=true;
+    if(st==true)
+    {
+        i=0;
+        fstream qst;
+        qst.open(Movies,ios::in | ios::out |ios::trunc);
+        for(auto itr = q.begin();itr!=q.end();itr ++,i++)
+        {
+            qst <<"name:"<<(*itr).name<<"/"
+               <<"director:"<<(*itr).director<<"/"
+              <<"actors:"<<(*itr).actors.at(i)<<"/"
+             <<"year:"<<(*itr).year<<"/"
+            <<"capacity:"<<(*itr).capacity<<"/"
+            <<endl;
+
+        }
+        st=false;
+    }
+}
+
+return 0;
 }
